7465ac771b9b73df6c176c02fab5115b6984b237
Siu <a17dc545@dhw.ac.jp>
2018-08-23 19:28:11 +0900

    Updated VectorMath.ts

--- src/math/VectorMath.ts
+++ src/math/VectorMath.ts
@@ -4,16 +4,29 @@ type Vector2 = { x: number, y: number };
 
 export default class VectorMath extends TimesteppedScene {
 
-	ResetVelocityWithinTime(vector: Vector2, speed: number) {
-		if (vector.x > 0) vector.x -= speed;
-		else if (vector.x < 0) vector.x += speed;
-		if (vector.y > 0) vector.y -= speed;
-		else if (vector.y < 0) vector.y += speed;
+	ResetVelocityWithinTime(vector: Vector2, speed: number, speed2?: number) {
+		if(speed2 == null){
+			if (vector.x > 0) vector.x -= speed;
+			else if (vector.x < 0) vector.x += speed;
+			if (vector.y > 0) vector.y -= speed;
+			else if (vector.y < 0) vector.y += speed;
 
-		if (vector.x > 0 && vector.x < speed) vector.x = 0;
-		else if (vector.x < 0 && vector.x > speed) vector.x = 0;
-		if (vector.y > 0 && vector.y < speed) vector.y = 0;
-		else if (vector.y < 0 && vector.y > speed) vector.y = 0;
+			if (vector.x > 0 && vector.x < speed) vector.x = 0;
+			else if (vector.x < 0 && vector.x > speed) vector.x = 0;
+			if (vector.y > 0 && vector.y < speed) vector.y = 0;
+			else if (vector.y < 0 && vector.y > speed) vector.y = 0;
+		}
+		else{
+			if (vector.x > 0) vector.x -= speed;
+			else if (vector.x < 0) vector.x += speed;
+			if (vector.y > 0) vector.y -= speed2;
+			else if (vector.y < 0) vector.y += speed2;
+
+			if (vector.x > 0 && vector.x < speed) vector.x = 0;
+			else if (vector.x < 0 && vector.x > speed) vector.x = 0;
+			if (vector.y > 0 && vector.y < speed2) vector.y = 0;
+			else if (vector.y < 0 && vector.y > speed2) vector.y = 0;
+		}
 	}
 
 }
