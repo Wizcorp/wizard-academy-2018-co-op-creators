6ad8e68b41e41f5c66e89e39620609ae0a428a21
Patirck <qwcwxc@gmail.com>
2018-08-24 15:11:03 +0900

    stage 1

---
+++ assets/Tilemap/Tiled-files/stage1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="stage1" tilewidth="8" tileheight="8" tilecount="8040" columns="120">
+ <image source="stage1.png" width="960" height="540"/>
+</tileset>
