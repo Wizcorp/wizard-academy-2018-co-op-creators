e81fb918a13cca3573ab30aa7cc2972c5ce067c4
Patirck <qwcwxc@gmail.com>
2018-08-28 14:53:29 +0900

    playerdeath animation

---
+++ assets/Tilemap/Tiled-files/spike.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="spike" tilewidth="8" tileheight="8" tilecount="16" columns="4">
+ <image source="../../obstacle/spike.png" width="32" height="32"/>
+</tileset>
